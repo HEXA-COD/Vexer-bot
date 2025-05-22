288110530125c518dcf4a344e16057224a8e9b25
HEXA-COD <kinggaccess@gmail.com>
2025-05-22 22:59:17 +0545

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
 const figlet = require('figlet');
-const ora = require('ora');
+const ora = require('ora').default;
 const moment = require('moment-timezone');
 
 // Load config
